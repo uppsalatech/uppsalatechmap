f3a06db6bbb27bd1ace19dbd54c5323babe067a4
Anders Åberg <anders.aberg@caspeco.se>
2019-06-19 12:52:41 +0200

    Cleaned up

--- src/pages/companies.js
+++ src/pages/companies.js
@@ -4,14 +4,10 @@ import { Link } from "gatsby"
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 
-//import Companies from "/companies.json";
-
-//console.log("DATA", Companies);
-
 const IndexPage = ({ pageContext }) => (
   <Layout>
     <SEO title="Home" />
-    <h2>Discover the tech companies in Uppsala</h2>
+    <h2>Discover tech companies in Uppsala</h2>
 
     <div className="cards">
       {pageContext.companies && pageContext.companies.filter((v) => v.name).map((v, i) => {
@@ -26,16 +22,6 @@ const IndexPage = ({ pageContext }) => (
     <div style={{marginTop:30, textAlign: "center"}}>
       <a href="https://github.com/uppsalatech/uppsalatechmap/edit/master/companies.json">Add your own company to the list</a>
     </div>
-
-
-    {/* {JSON.stringify(pageContext)} */}
-
-
-    {/* <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
-    </div>
-     */}
-    {/* <Link to="/page-2/">Go to page 2</Link> */}
   </Layout>
 )
 
