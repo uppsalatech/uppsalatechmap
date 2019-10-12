1a2ab1f8a98788bebf0f1f009f964a1588ab04d2
Emma Uddesson <emma.uddesson@gmail.com>
2019-10-12 22:09:07 +0200

    Add draft for displaying companies in a map component

---
+++ src/components/map.js
@@ -0,0 +1,59 @@
+import React from "react"
+import PropTypes from "prop-types"
+
+import { Map as LeafletMap, TileLayer, Marker, Popup } from "react-leaflet";
+
+class Map extends React.Component {
+  constructor(props) {
+    super()
+    this.state = {
+      // Position for Uppsala city center-ish
+      lat: 59.858266,
+      lng: 17.638184,
+      zoom: 14,
+    }
+  }
+
+  getCompanyMarkers = () => {
+    console.log(typeof this.props.companies)
+    // Fallback if no companies are loaded
+    if (!this.props.companies) return <Marker position={[this.state.lat, this.state.lng]} />;
+
+    return this.props.companies.map((company) => {
+        if (Boolean(company.lat) && Boolean(company.lng)) {
+          return (
+            <Marker position={[company.lat, company.lng]}>
+              <Popup>
+                {company.name}
+              </Popup>
+            </Marker>
+            )
+        }
+        return null;
+    })
+  }
+
+  render() {
+    const position = [this.state.lat, this.state.lng];
+    if (typeof window !== 'undefined') {
+      return (
+        <div className="map-container">
+          <LeafletMap center={position} zoom={this.state.zoom}>
+            <TileLayer
+              attribution="&amp;copy <a href=&quot;http://osm.org/copyright&quot;>OpenStreetMap</a> contributors"
+              url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+            />
+            {this.getCompanyMarkers()}
+          </LeafletMap>
+        </div>
+      )
+    }
+    return null;
+  }
+}
+
+Map.propTypes = {
+  companies: PropTypes.object
+};
+
+export default Map
--- src/pages/companies.js
+++ src/pages/companies.js
@@ -3,13 +3,16 @@ import { Link } from "gatsby"
 
 import Layout from "../components/layout"
 import SEO from "../components/seo"
+import Map from "../components/map"
 
 const IndexPage = ({ pageContext }) => (
   <Layout>
     <SEO title="Home" />
     <h2 style={{marginTop: "2rem"}}>Discover tech companies in Uppsala</h2>
     <p>Join the conversation in the Uppsala Tech Community Slack by emailing <a href="mailto:hello@uppsalatech.se">hello@uppsalatech.se</a>.</p>
-
+    
+    <Map companies={pageContext.companies} />
+    
     <div className="cards">
       {pageContext.companies && pageContext.companies.filter((v) => v.name).map((v, i) => {
         return (<div className="card" key={i}>
