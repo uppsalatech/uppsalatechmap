a8bda6443d25611ad02f02d6528aa0fcb2408783
Emma Uddesson <emma.uddesson@gmail.com>
2022-05-24 11:57:45 +0200

    Show company locations on interactive map (#2)
    * Add draft for displaying companies in a map component
    
    * Add test locations for two companies
    
    * Display anchor links in marker tooltips
    
    * Add more company locations
    
    * Remove spacing in company names when used for id
    
    * Decrease initial zoom level for map
    
    * Remove console.log
    
    * Add custom marker icon
    
    * Remove anchorlink
    
    Clashed with hover animation, misleading
    
    * Tweak map styles

--- src/pages/companies.js
+++ src/pages/companies.js
@@ -3,16 +3,20 @@ import { Link } from "gatsby"
 
 import Layout from "../components/layout"
 import SEO from "../components/seo"
+import Map from "../components/map"
 
 const IndexPage = ({ pageContext }) => (
   <Layout>
     <SEO title="Home" />
     <h2 style={{marginTop: "2rem"}}>Discover tech companies in Uppsala</h2>
     <p>Join the conversation in the Uppsala Tech Community Slack by emailing <a href="mailto:hello@uppsalatech.se">hello@uppsalatech.se</a>.</p>
-
+    
+    <Map companies={pageContext.companies} />
+    
     <div className="cards">
       {pageContext.companies && pageContext.companies.filter((v) => v.name).map((v, i) => {
-        return (<div className="card" key={i}>
+        const id = v.name.replace(/\s/g, '');
+        return (<div className="card" key={i} id={id}>
           <a href={v.url} className="name">{v.name}</a>
           <p>{v.description}</p>
           { v.jobs_url && <a href={v.jobs_url} className="jobs_url">Open positions</a>}
